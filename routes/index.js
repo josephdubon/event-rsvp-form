4a7783f4a7a2c77adbaa10c2cc3306640917c823
Joseph Dubon <josephdubon@pm.me>
2020-09-30 16:08:51 -0700

    Created form and updated index page with links to other pages.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.render('index', { title: 'Assessment - Event RSVP Form' })
 })
 
 module.exports = router
